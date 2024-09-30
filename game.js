dd7c090b71ec3a67956a3764a9b6ac32d1e54aad
Ewoma Akwenuke <ewomaakwenuke@gmail.com>
2024-09-30 07:03:05 +0100

    Update JS: Add game over animation, sound, and reset functionality

--- game.js
+++ game.js
@@ -1,19 +1,16 @@
-var buttonSequence = ["red", "blue", "green", "yellow" ];
+var buttonSequence = [ "red", "blue", "green", "yellow" ];
 
 var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
-
 var level = 0
 
 $(document).keypress( function() { 
    if (!started){
-    
     $("#level-title").text("Level" + level)
     nextSequence();
     started = true;
-    level ++;
 }
 });
 
@@ -27,16 +24,42 @@ $(".btn").click(function () {
 
     playSound(userChosenColor);
     animatePress(userChosenColor)
-    checkAnswer()
+    checkAnswer(userClickedPattern.length-1);
 
 });
 
 
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+            
+        }
+
+    } else {
+
+        console.log("wrong");
+        playSound("wrong");
+
+        $("body").addClass("game-over")
+          window.setTimeout(function(){
+            $("body").removeClass("game-over")
+          }, 200)
+
+         $("#level-title").text("Game over, Press any key to restart")
+    
+    }
+
+} 
 
 function nextSequence() {
 
+    userClickedPattern = [];
     level++;
-
     $("#level-title").text("Level " + level);
 
    var randomNumber = Math.floor(Math.random() * 4);
@@ -71,10 +94,7 @@ function animatePress(currentColor) {
         100
     );
 }
-
-function checkAnswer(currentlevel) {
-    
-}
+ 
 
 
 
